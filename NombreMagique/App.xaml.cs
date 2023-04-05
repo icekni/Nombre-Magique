2c4c088378f6953d02b54e305bc9e8154c36fe62
icekni <cjosso@gmail.com>
2023-04-05 13:54:24 +0200

    logique du jeu

---
+++ NombreMagique/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using NombreMagique.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NombreMagique
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new GamePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
